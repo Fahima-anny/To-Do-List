d1e4fa9f1b1c12537630372466a656b415ec4810
Fahima-anny <fahima.info03@gmail.com>
2024-03-01 01:38:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+
+
+ let table = document.querySelector("table")
+
+let doneBtn = document.getElementById("doneBtn")
+doneBtn.addEventListener("click",function(){
+   document.getElementById("sec1").style.display = "none"
+   document.getElementById("sec2").style.display = "block"
+
+let tm1 = document.getElementById("tm1")
+let DIVtm1 = document.getElementById("DIVtm1")
+DIVtm1.innerText = tm1.value 
+
+let tm2 = document.getElementById("tm2")
+let DIVtm2 = document.getElementById("DIVtm2")
+DIVtm2.innerText = tm2.value 
+
+let tm3 = document.getElementById("tm3")
+let DIVtm3 = document.getElementById("DIVtm3")
+DIVtm3.innerText = tm3.value 
+
+let tm4 = document.getElementById("tm4")
+let DIVtm4 = document.getElementById("DIVtm4")
+DIVtm4.innerText = tm4.value 
+
+let tm5 = document.getElementById("tm5")
+let DIVtm5 = document.getElementById("DIVtm5")
+DIVtm5.innerText = tm5.value 
+
+let tm6 = document.getElementById("tm6")
+let DIVtm6 = document.getElementById("DIVtm6")
+DIVtm6.innerText = tm6.value 
+
+let tm7 = document.getElementById("tm7")
+let DIVtm7 = document.getElementById("DIVtm7")
+DIVtm7.innerText = tm7.value 
+
+
+
+
+let ac1 = document.getElementById("ac1")
+let DIVac1 = document.getElementById("DIVac1")
+DIVac1.innerText = ac1.value 
+
+let ac2 = document.getElementById("ac2")
+let DIVac2 = document.getElementById("DIVac2")
+DIVac2.innerText = ac2.value 
+
+let ac3 = document.getElementById("ac3")
+let DIVac3 = document.getElementById("DIVac3")
+DIVac3.innerText = ac3.value 
+
+let ac4 = document.getElementById("ac4")
+let DIVac4 = document.getElementById("DIVac4")
+DIVac4.innerText = ac4.value 
+
+let ac5 = document.getElementById("ac5")
+let DIVac5 = document.getElementById("DIVac5")
+DIVac5.innerText = ac5.value 
+
+let ac6 = document.getElementById("ac6")
+let DIVac6 = document.getElementById("DIVac6")
+DIVac6.innerText = ac6.value 
+
+let ac7 = document.getElementById("ac7")
+let DIVac7 = document.getElementById("DIVac7")
+DIVac7.innerText = ac7.value 
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
